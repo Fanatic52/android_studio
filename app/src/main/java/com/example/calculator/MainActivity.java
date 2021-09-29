d0f59f65df9177bb12938fcd3c90aa5d345b15bf
Fanatic52 <71984863+Fanatic52@users.noreply.github.com>
2021-09-29 15:20:20 +0300

    update

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -28,13 +28,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     String oper = "";
 
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // находим элементы
         etNum1 = (EditText) findViewById(R.id.etNum1);
         etNum2 = (EditText) findViewById(R.id.etNum2);
 
@@ -45,7 +43,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         tvResult = (TextView) findViewById(R.id.tvResult);
 
-        // прописываем обработчик
         btnAdd.setOnClickListener(this);
         btnSub.setOnClickListener(this);
         btnMult.setOnClickListener(this);
@@ -55,23 +52,18 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        // TODO Auto-generated method stub
         float num1 = 0;
         float num2 = 0;
         float result = 0;
 
-        // Проверяем поля на пустоту
         if (TextUtils.isEmpty(etNum1.getText().toString())
                 || TextUtils.isEmpty(etNum2.getText().toString())) {
             return;
         }
 
-        // читаем EditText и заполняем переменные числами
         num1 = Float.parseFloat(etNum1.getText().toString());
         num2 = Float.parseFloat(etNum2.getText().toString());
 
-        // определяем нажатую кнопку и выполняем соответствующую операцию
-        // в oper пишем операцию, потом будем использовать в выводе
         switch (v.getId()) {
             case R.id.btnAdd:
                 oper = "+";
@@ -93,7 +85,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 break;
         }
 
-        // формируем строку вывода
         tvResult.setText(num1 + " " + oper + " " + num2 + " = " + result);
     }
 
@@ -104,18 +95,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
         return super.onCreateOptionsMenu(menu);
     }
 
-    // обработка нажатий на пункты меню
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case MENU_RESET_ID:
-                // очищаем поля
+               
                 etNum1.setText("");
                 etNum2.setText("");
                 tvResult.setText("");
                 break;
             case MENU_QUIT_ID:
-                // выход из приложения
                 finish();
                 break;
         }
